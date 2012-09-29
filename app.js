373602818936966c757f34f0d306c0e154e40227
Stephen Rivas JR <stephen@zeunic.com>
2012-09-29 15:06:48 -0400

    Readddyyy....GO!

--- app.js
+++ app.js
@@ -4,32 +4,46 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+	, routes = require('./routes')
+	, http = require('http')
+	, path = require('path');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
+});
+
+app.param(':adventureID', function(req, res, next, adventureID){
+	req.adventureID = adventureID.substring(2, adventureID.length-3);
+
+	console.log('app param first?');
+	console.log(adventureID);
+	console.log(req.adventureID);
+
+	if(req.adventureID) {
+		next();
+	} else {
+		// to do need error page
+		console.log('render error page');
+	}
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/adventures/:adventureID', routes.adventure);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,34 @@
  * GET home page.
  */
 
+ var request = require('request'),
+	hash = require('node_hash');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.render('home', { title: 'Globl.me' });
+};
+
+exports.adventure = function(req,res){
+	var requestData = {
+			key: "globlme",
+			auth: '',
+			data: {
+				"unique": Math.random(),
+				"type": "adventure",
+				"moments": "true"
+			}
+		};
+
+	requestData.auth = hash.sha256( JSON.stringify( requestData.data ) + '3912b4f3-c9ab-4e2f-9007-424b5b1d5de6');
+
+	var requestOptions = {
+		uri: 'http://dev.globl.me/v1.1/collection/' + req.adventureID,
+		method: 'POST',
+		json: requestData
+	};
+
+	request(requestOptions, function(err, response, result){
+		res.render('index', { title: 'Adventure: ' + result.data.title, data: JSON.stringify(result) });
+	});
+	
 };
